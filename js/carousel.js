d406eb3ff516dd7906c974f7605bfa30aed0642c
LyKimThanh <93457194+LyKimThanh@users.noreply.github.com>
2023-01-14 17:00:03 -0500

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -86,3 +86,18 @@ dotsNav.addEventListener('click', e => {
 
 
 })
+
+
+
+/////////////////////////////////////////
+const btnnav = document.querySelector('.icon-nav-open');
+const btnnav2 = document.querySelector('.icon-nav-close');
+
+const header = document.querySelector('.header');
+btnnav.addEventListener('click', function(){
+    header.classList.add('nav-open');
+});
+btnnav2.addEventListener('click', function(){
+    header.classList.remove('nav-open');
+});
+
